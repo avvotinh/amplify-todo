57ff140449f74175984e44e0cf30c7b7943b041e
Le Van Hop <hop.le@axonactive.com>
2019-08-27 18:11:27 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
+
+Amplify.configure(awsconfig);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [
+        { name: "Clean the house", status: "NEW" },
+        { name: "Learn the cloud", status: "NEW" }
+      ]
+    };
+  }
+
+  logout = () => {
+    Auth.signOut();
+    window.location.reload();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <main>
+          <h1>TODO List</h1>
+          <TodoList items={this.state.items} />
+        </main>
+
+        <button onClick={this.logout} title="Log out">Log out</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+class TodoList extends Component {
+  render() {
+    return (
+      <div className="TodoList">
+        <ul>
+          {this.props.items.map((item, index) => {
+            return <TodoItem item={item} key={index} />;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+class TodoItem extends Component {
+  render() {
+    const item = this.props.item;
+
+    return (
+      <li>
+        <input type="checkbox" />
+        {item.name}
+      </li>
+    );
+  }
+}
+
+export default withAuthenticator(App);
